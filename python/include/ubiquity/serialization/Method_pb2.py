ac0299e7711e576cb429ec1610dc197ee5a68e6a
Andrea F. Daniele <afdaniele@ttic.edu>
2020-02-12 02:25:46 -0600

    minor

--- python/include/ubiquity/serialization/Method_pb2.py
+++ python/include/ubiquity/serialization/Method_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='',
   syntax='proto3',
   serialized_options=None,
-  serialized_pb=b'\n#ubiquity/serialization/Method.proto\x1a\x19google/protobuf/any.proto\"4\n\x08MethodPB\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x1a\n\x04\x61rgs\x18\x02 \x03(\x0b\x32\x0c.ParameterPB\"\xa1\x01\n\x0bParameterPB\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x1e\n\x04type\x18\x02 \x01(\x0e\x32\x10.ParameterTypePB\x12\x12\n\nannotation\x18\x03 \x01(\t\x12+\n\rdefault_value\x18\x04 \x01(\x0b\x32\x14.google.protobuf.Any\x12#\n\x05value\x18\x05 \x01(\x0b\x32\x14.google.protobuf.Any*`\n\x0fParameterTypePB\x12\x0b\n\x07\x44\x45\x46\x41ULT\x10\x00\x12\x0e\n\nPOSITIONAL\x10\x02\x12\x0b\n\x07KEYWORD\x10\x03\x12\x12\n\x0eVAR_POSITIONAL\x10\x04\x12\x0f\n\x0bVAR_KEYWORD\x10\x05\x62\x06proto3'
+  serialized_pb=b'\n#ubiquity/serialization/Method.proto\x1a\x19google/protobuf/any.proto\"4\n\x08MethodPB\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x1a\n\x04\x61rgs\x18\x02 \x03(\x0b\x32\x0c.ParameterPB\"\xa1\x01\n\x0bParameterPB\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x1e\n\x04type\x18\x02 \x01(\x0e\x32\x10.ParameterTypePB\x12\x12\n\nannotation\x18\x03 \x01(\t\x12+\n\rdefault_value\x18\x04 \x01(\x0b\x32\x14.google.protobuf.Any\x12#\n\x05value\x18\x05 \x01(\x0b\x32\x14.google.protobuf.Any*`\n\x0fParameterTypePB\x12\x0b\n\x07\x44\x45\x46\x41ULT\x10\x00\x12\x0e\n\nPOSITIONAL\x10\x01\x12\x0b\n\x07KEYWORD\x10\x02\x12\x12\n\x0eVAR_POSITIONAL\x10\x03\x12\x0f\n\x0bVAR_KEYWORD\x10\x04\x62\x06proto3'
   ,
   dependencies=[google_dot_protobuf_dot_any__pb2.DESCRIPTOR,])
 
@@ -35,19 +35,19 @@ _PARAMETERTYPEPB = _descriptor.EnumDescriptor(
       serialized_options=None,
       type=None),
     _descriptor.EnumValueDescriptor(
-      name='POSITIONAL', index=1, number=2,
+      name='POSITIONAL', index=1, number=1,
       serialized_options=None,
       type=None),
     _descriptor.EnumValueDescriptor(
-      name='KEYWORD', index=2, number=3,
+      name='KEYWORD', index=2, number=2,
       serialized_options=None,
       type=None),
     _descriptor.EnumValueDescriptor(
-      name='VAR_POSITIONAL', index=3, number=4,
+      name='VAR_POSITIONAL', index=3, number=3,
       serialized_options=None,
       type=None),
     _descriptor.EnumValueDescriptor(
-      name='VAR_KEYWORD', index=4, number=5,
+      name='VAR_KEYWORD', index=4, number=4,
       serialized_options=None,
       type=None),
   ],
@@ -60,10 +60,10 @@ _sym_db.RegisterEnumDescriptor(_PARAMETERTYPEPB)
 
 ParameterTypePB = enum_type_wrapper.EnumTypeWrapper(_PARAMETERTYPEPB)
 DEFAULT = 0
-POSITIONAL = 2
-KEYWORD = 3
-VAR_POSITIONAL = 4
-VAR_KEYWORD = 5
+POSITIONAL = 1
+KEYWORD = 2
+VAR_POSITIONAL = 3
+VAR_KEYWORD = 4
 
 
 
